02fd9ef82f607ad66801a462552c8b1b6d77c86a
jinrunli <ljr@y-clouds.com>
2022-09-04 17:29:54 +0800

    change: Change #3 清理不必要的参数

--- cmd/root.go
+++ cmd/root.go
@@ -41,11 +41,11 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.watch-nl.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.rattler/config.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 }
 
